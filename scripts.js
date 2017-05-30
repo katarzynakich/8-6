16462052accd3ae0cb70a0e5948a89bbb912b322
katarzynakich <katarzynakich@gmail.com>
2017-05-31 00:26:02 +0200

    corrected

--- scripts.js
+++ scripts.js
@@ -3,14 +3,14 @@ alert("Policzmy (a * a) + (2 * a * b) - (b * b)");
 var a = prompt("Podaj liczbę a:");
 var b = prompt("Podaj liczbę b:");
 var value = (a * a) + (2 * a * b) - (b * b);
-alert("Wynik sprawdź w konsoli :)")
+alert("Wynik sprawdź w konsoli :)");
 console.log(value);
 
 if (value > 0) {
 	console.log("Wynik dzialania (a * a) + (2 * a * b) - (b * b) dla a: " + a + " i b: " + b + " jest dodatni i wynosi: " + value);
 } else if (value < 0) {
    console.log("Wynik dzialania (a * a) + (2 * a * b) - (b * b) dla a: " + a + " i b: " + b + " jest ujemny i wynosi: " + value);
-} else if (value = 0) {
+} else if (value === 0) {
 	console.log("Wynik dzialania (a * a) + (2 * a * b) - (b * b) dla a: " + a + " i b: " + b + "  wynosi 0");
 } else {
 console.log("Nie podano liczb. Spróbuj ponownie :)");
